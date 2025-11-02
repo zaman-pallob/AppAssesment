3cea3cdddfea4ead5267c508c7cce68dee7958b7
Zaman-Pallob <atikurzamanpallob@gmail.com>
2025-11-02 13:10:36 +0600

    failures added

--- lib/dependency/di.dart
+++ lib/dependency/di.dart
@@ -1,4 +1,10 @@
+import 'package:app_assesment/core/networks/client.dart';
+import 'package:app_assesment/core/networks/network_info.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt dependency = GetIt.instance;
-Future injectDependecy() async {}
+Future injectDependecy() async {
+  dependency.registerLazySingleton<Dio>(() => Client.createClient());
+  dependency.registerLazySingleton<NetworkInfoImpl>(() => NetworkInfoImpl());
+}
